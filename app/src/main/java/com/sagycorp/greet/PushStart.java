ff1bda497ad141135735b95cda4a4610fa0e5186
Marcostar <sagarghadge24@gmail.com>
2015-11-05 19:18:13 +0530

    PushN

---
+++ app/src/main/java/com/sagycorp/greet/PushStart.java
@@ -0,0 +1,20 @@
+package com.sagycorp.greet;
+
+import android.app.Application;
+
+import com.parse.Parse;
+import com.parse.ParseInstallation;
+
+/**
+ * Created by Dzeko on 11/5/2015.
+ */
+public class PushStart extends Application {
+
+    @Override
+    public void onCreate()
+    {
+        super.onCreate();
+        Parse.initialize(this, "bLwuq7MBXbyuBSrzmuw7Kkl2vUGb6rpszbEsanIO", "LAGZVb7E473d56HjqkczculReTkBZyUcwkcU3JTs");
+        ParseInstallation.getCurrentInstallation().saveInBackground();
+    }
+}
